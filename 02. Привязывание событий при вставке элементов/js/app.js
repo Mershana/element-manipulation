5ca0e758d91be649e15031d84b6b05811340948b
Mershana <kriomaas@gmail.com>
2022-11-02 17:53:35 +0300

    02. Привязывание событий при вставке элементов

---
+++ 02. Привязывание событий при вставке элементов/js/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Привязывание событий при вставке элементов
+/* №1⊗jsPmMnIBE
+Дан ol и кнопка. Сделайте так, чтобы по клику на кнопку в конец ol добавлялся тег li. Сделайте так, чтобы по клику на любой из добавленных li в конец его текста записывался восклицательный знак. */
+
+/* let elems = document.querySelector('#elem');
+let button = document.querySelector('#button');
+let li = document.createElement('li');
+
+
+button.addEventListener('click', function(){
+		li.textContent = 'text';
+		elems.appendChild(li);
+});
+
+li.addEventListener('click', function(){
+	li.textContent += '!';
+}) */
