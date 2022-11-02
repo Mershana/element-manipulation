ead5b6257f4027e0b60864060fe8bea4ce73f2ef
Mershana <kriomaas@gmail.com>
2022-11-02 17:54:02 +0300

    04. Навешивание обработчиков в цикле

---
+++ 04. Навешивание обработчиков в цикле/js/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+
+// Навешивание обработчиков в цикле на JavaScript
+
+// №1⊗jsPmMnLBH
+// Дан див. Запустите цикл, который добавит в наш див 5 инпутов. Пусть дан также абзац. Сделайте так, чтобы каждый из новых инпутов по потери фокуса записывал свой текст в абзац.
+/* let elems = document.querySelector('#elem');
+let paragraph = document.querySelector('p');
+for (let i = 1; i <= 5; i++){
+	let input = document.createElement('input');
+	input.value= 'text';
+
+	input.addEventListener('blur', function(){
+		paragraph.textContent = input.value;
+	});
+
+	elems.appendChild(input);
+} */
